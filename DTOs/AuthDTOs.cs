c431579f4cbace10f6bed5c6278c1930c967e7a3
FarhazR <rahmanfarhaz64@gmail.com>
2026-06-23 16:27:26 +0530

    ngrok zip added, some other changes to login (via username)

--- DTOs/AuthDTOs.cs
+++ DTOs/AuthDTOs.cs
@@ -2,7 +2,8 @@ namespace CanteenAPI.DTOs
 {
     public class LoginRequest
     {
-        public int EmployeeID { get; set; }
+        public int? EmployeeID { get; set; }
+        public string? Username { get; set; }
         public string Password { get; set; } = string.Empty;
     }
 
@@ -10,6 +11,7 @@ namespace CanteenAPI.DTOs
     {
         public string Token { get; set; } = string.Empty;
         public int EmployeeID { get; set; }
+        public string Username { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Department { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -4,6 +4,7 @@ namespace CanteenAPI.Models
     {
         public int EmployeeID { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string Username { get; set;} = string.Empty;
         public string Department { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string Role { get; set; } = "Employee";
