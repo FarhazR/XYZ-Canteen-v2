6dec387cc7b5456bfb9c7f57bd0b09eeeff2b13f
FarhazR <rahmanfarhaz64@gmail.com>
2026-07-17 15:05:34 +0530

    Migration mismatch with database fixed

--- Migrations/20260717091759_AddAddOnsAndBookingAddOns.cs
+++ Migrations/20260717091759_AddAddOnsAndBookingAddOns.cs
@@ -11,13 +11,7 @@ namespace CanteenAPI.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<Guid>(
-                name: "BookingGroupID",
-                table: "Bookings",
-                type: "uniqueidentifier",
-                nullable: false,
-                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
-
+    
             migrationBuilder.CreateTable(
                 name: "AddOns",
                 columns: table => new
